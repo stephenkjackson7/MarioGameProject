f73cc4e34c6cee6ae75c1ea759045b6f785b38f4
Stephen JACKSON <j018873k@student.staffs.ac.uk>
2021-03-31 14:33:01 +0100

    tut5

---
+++ MarioGame/MarioGame/Commons.h
@@ -0,0 +1,28 @@
+#pragma once
+
+struct Vector2D
+{
+	float x;
+	float y;
+
+	Vector2D()
+	{
+		x = 0.0f;
+		y = 0.0f;
+	};
+
+};
+
+struct InitVector2D
+{
+
+	float x;
+	float y;
+
+	InitVector2D(float initial_x, float initial_y)
+	{
+		x = initial_x;
+		y = initial_y;
+	}
+};
+
